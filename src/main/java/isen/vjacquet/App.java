56b77ed631b14dbe7112557104ff5fa32c6cc50a
Valentin Jacquet <vlt.jcqt@gmail.com>
2017-10-22 17:39:40 +0200

    Add pom.xml

---
+++ src/main/java/isen/vjacquet/App.java
@@ -0,0 +1,13 @@
+package isen.vjacquet;
+
+/**
+ * Hello PA165!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello PA165!" );
+    }
+}
